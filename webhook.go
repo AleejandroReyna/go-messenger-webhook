7cefb2613df0034f915870059314c8a62faac867
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-11-24 17:11:33 -0600

    Added token and godotenv for messenger webhook

--- webhook.go
+++ webhook.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func getEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
 
 func main() {
-	fmt.Println("hello webhook")
+	token := getEnvVariable("TOKEN")
+	fmt.Println(token)
 }
