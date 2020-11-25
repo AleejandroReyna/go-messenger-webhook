cc048b78d145b03d95025a2d0c01f1b095b57a7a
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-11-24 18:13:08 -0600

    Added verify webhook

--- webhook.go
+++ webhook.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -16,7 +17,25 @@ func getEnvVariable(key string) string {
 	return os.Getenv(key)
 }
 
+type HTTPHandler struct{}
+
+func (h HTTPHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	requestToken := req.URL.Query().Get("hub.verify_token")
+	requestChallenge := req.URL.Query().Get("hub.challenge")
+	var data []byte
+
+	if requestToken == getEnvVariable("WEBHOOK_TOKEN") {
+		data = []byte(requestChallenge)
+		fmt.Println("Accepted")
+	} else {
+		data = []byte("No Permissions")
+		fmt.Println("Denegate")
+	}
+
+	res.Write(data)
+}
+
 func main() {
-	token := getEnvVariable("TOKEN")
-	fmt.Println(token)
+	handler := HTTPHandler{}
+	http.ListenAndServe(":9000", handler)
 }
